1f53361510eaa8599853aa54f7e0c765ae1e0551
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-11 17:32:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
   i = 0
   while i < src.length do
     j = 0
-    local_min = src[0][0] #set first local min default to first element of innermost array
+    local_min = src[i][0] #set first local min default to first element of innermost array
     while j < src[i].length do
       if src[i][j] < local_min
         local_min = src[i][j]
